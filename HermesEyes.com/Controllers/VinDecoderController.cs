e58a8d01d72f0a7f3f170f88fbb9c5b80d21f7cc
Daniel WOAGOU <daniwx@gmail.com>
2022-04-20 22:19:35 +0000

    Securisation decodage

--- HermesEyes.com/Controllers/VinDecoderController.cs
+++ HermesEyes.com/Controllers/VinDecoderController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HermesEyes.com.Model;
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using Services.DataServices;
@@ -10,15 +12,19 @@ namespace HermesEyes.com.Controllers
     public class VinDecoderController : ControllerBase
     {
         private readonly VinRushScrapper vinRushScrapper;
-
-        public VinDecoderController(VinRushScrapper vinRushScrapper)
+        private readonly TokensProvider _tokensprovider;
+        public VinDecoderController(TokensProvider tokensProvider, VinRushScrapper vinRushScrapper)
         {
             this.vinRushScrapper = vinRushScrapper;
         }
         [HttpGet()]
         [HttpPost]
-        public async Task<ActionResult<Dictionary<string, string>>> Decode (string vin)
+        public async Task<ActionResult<Dictionary<string, string>>> Decode (string token, string vin)
         {
+            if (token == null || !_tokensprovider.IsValid(token))
+            {
+                return Unauthorized(new MarketValueResponse("Token Invalid"));
+            }
             vinRushScrapper.Vin = vin;
             return Ok (vinRushScrapper.IdentifyCarByVINAsync(vin));
         }
